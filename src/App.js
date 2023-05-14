eace0aab9b4dca966c0d2b23035738c5d0e10342
thewronghand <penfreak77@gmail.com>
2023-05-14 21:57:13 +0900

    feat: Router를 통한 페이지 구성 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import { RecoilRoot } from "recoil";
+import { BrowserRouter as Switch, Route } from "react-router-dom";
+
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Main from "./Pages/Main";
+import ProductList from "./Pages/ProductList";
+import BookmarkList from "./Pages/BookmarkList";
 
 function App() {
   return (
     <RecoilRoot>
-      <></>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route path="/products/list">
+          <ProductList />
+        </Route>
+        <Route path="/bookmark">
+          <BookmarkList />
+        </Route>
+      </Switch>
+      <Footer />
     </RecoilRoot>
   );
 }
