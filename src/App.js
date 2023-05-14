bb339284d38b550979cdf40cb57196903b1821d9
thewronghand <penfreak77@gmail.com>
2023-05-14 23:04:14 +0900

    init: initialize coz-shopping project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { RecoilRoot } from "recoil";
 import { Routes, Route } from "react-router-dom";
 
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
 import Main from "./Pages/Main";
 import ProductList from "./Pages/ProductList";
 import BookmarkList from "./Pages/BookmarkList";
@@ -10,13 +8,11 @@ import BookmarkList from "./Pages/BookmarkList";
 function App() {
   return (
     <RecoilRoot>
-      <Header />
       <Routes>
         <Route exact path="/" element={<Main />} />
         <Route path="/products/list" element={<ProductList />} />
         <Route path="/bookmark" element={<BookmarkList />} />
       </Routes>
-      <Footer />
     </RecoilRoot>
   );
 }
