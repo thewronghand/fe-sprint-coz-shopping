c16f4e5e65bba832814eac140bf733f8c2aef2d6
thewronghand <penfreak77@gmail.com>
2023-05-16 04:48:38 +0900

    feat: Card, Bookmark 컴포넌트 로직 수정

--- src/Components/Bookmark.js
+++ src/Components/Bookmark.js
@@ -1,37 +1,37 @@
-import { useEffect, useState } from "react";
+// Bookmark.js
+import { useEffect } from "react";
 
-function Bookmark({ data }) {
-  const [isBookmarked, setIsBookmarked] = useState(false);
-
+function Bookmark({ data, isBookmarked, setIsBookmarked }) {
   useEffect(() => {
     checkBookmarkStatus();
-  }, []);
+  }, [data]);
 
   const checkBookmarkStatus = () => {
     const bookmarks = JSON.parse(localStorage.getItem("bookmark") || "[]");
-    const isDataBookmarked = bookmarks.includes(data);
+    const isDataBookmarked = bookmarks.some(
+      (bookmark) => bookmark.id === data.id
+    );
 
     setIsBookmarked(isDataBookmarked);
   };
 
   const toggleBookmark = () => {
     const bookmarks = JSON.parse(localStorage.getItem("bookmark") || "[]");
-
     if (isBookmarked) {
-      const updatedBookmarks = bookmarks.filter((item) => item.id !== data.id);
+      const updatedBookmarks = bookmarks.filter(
+        (bookmark) => bookmark.id !== data.id
+      );
       localStorage.setItem("bookmark", JSON.stringify(updatedBookmarks));
       setIsBookmarked(false);
     } else {
-      const updatedBookmarks = [data, ...bookmarks];
+      const updatedBookmarks = [...bookmarks, data];
       localStorage.setItem("bookmark", JSON.stringify(updatedBookmarks));
       setIsBookmarked(true);
     }
-
-    setIsBookmarked(!isBookmarked);
   };
 
   return (
-    <div>
+    <div style={{ cursor: "pointer" }}>
       {isBookmarked ? (
         <img src="bookmark-on.png" alt="bookmark-on" onClick={toggleBookmark} />
       ) : (
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,4 +1,7 @@
+import { useState, useEffect } from "react";
 import { styled } from "styled-components";
+
+import Bookmark from "./Bookmark";
 
 const CardContainer = styled.article`
   width: 264px;
@@ -6,6 +9,48 @@ const CardContainer = styled.article`
   min-width: 264px;
   min-height: 264px;
   border-radius: 12px;
+  > .modal-backdrop {
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(255, 255, 255, 0.5);
+    top: 0;
+    left: 0;
+    z-index: 1;
+    > .modal-overlay {
+      position: relative;
+      width: 744px;
+      height: 480px;
+      border-radius: 12px;
+      overflow: hidden;
+      > img {
+        width: 100%;
+        height: 100%;
+      }
+      > .modal-close {
+        position: absolute;
+        right: 20px;
+        top: 20px;
+        cursor: pointer;
+      }
+      > .modal-overlay_overlay {
+        position: absolute;
+        left: 24px;
+        bottom: 24px;
+      }
+      > .modal-overlay_title {
+        position: absolute;
+        left: 56px;
+        bottom: 24px;
+        font-weight: 700;
+        font-size: 24px;
+        color: white;
+      }
+    }
+  }
   > .image-container {
     width: 100%;
     height: 210px;
@@ -45,11 +90,52 @@ const CardContainer = styled.article`
   }
 `;
 
-function Card({ image, overlay, infoTop, infoBottom }) {
+function Card({ image, infoTop, infoBottom, data }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  const handleModalToggle = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const handleBookmarkUpdate = () => {
+    setIsBookmarked(!isBookmarked);
+  };
+
   return (
     <CardContainer>
-      <div className="image-container">
-        <div className="overlay">{overlay}</div>
+      {isModalOpen && (
+        <div className="modal-backdrop" onClick={handleModalToggle}>
+          <article
+            className="modal-overlay"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="modal-close" onClick={handleModalToggle}>
+              <img src="modal-close.png" alt="modal-close" />
+            </div>
+            <div className="modal-overlay_overlay">
+              <Bookmark
+                isBookmarked={isBookmarked}
+                setIsBookmarked={setIsBookmarked}
+                toggleBookmark={handleBookmarkUpdate}
+                data={data}
+              />
+            </div>
+            <div className="modal-overlay_title">{data.title}</div>
+            {image}
+          </article>
+        </div>
+      )}
+
+      <div className="image-container" onClick={handleModalToggle}>
+        <div className="overlay" onClick={(e) => e.stopPropagation()}>
+          <Bookmark
+            isBookmarked={isBookmarked}
+            setIsBookmarked={setIsBookmarked}
+            toggleBookmark={handleBookmarkUpdate}
+            data={data}
+          />
+        </div>
         {image ? image : <div className="placeholder" />}
       </div>
 
